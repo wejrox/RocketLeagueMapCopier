94d17998350e9b39f86ab6e688c22bb0818334c9
James McDowell <pomy_jamie@hotmail.com>
2016-11-05 12:33:10 +1100

    First Commit

---
+++ RocketLeagueMapCopier/Main.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace RocketLeagueMapCopier
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnApplyMap = new System.Windows.Forms.Button();
+            this.txtMapName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkPackage1 = new System.Windows.Forms.CheckBox();
+            this.txtPackage1 = new System.Windows.Forms.TextBox();
+            this.txtPackage2 = new System.Windows.Forms.TextBox();
+            this.chkPackage2 = new System.Windows.Forms.CheckBox();
+            this.txtPackage3 = new System.Windows.Forms.TextBox();
+            this.chkPackage3 = new System.Windows.Forms.CheckBox();
+            this.txtPackage4 = new System.Windows.Forms.TextBox();
+            this.chkPackage4 = new System.Windows.Forms.CheckBox();
+            this.txtPackage5 = new System.Windows.Forms.TextBox();
+            this.chkPackage5 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblCopyResult = new System.Windows.Forms.Label();
+            this.txtModsDir = new System.Windows.Forms.TextBox();
+            this.txtMapDir = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnApplyMap
+            // 
+            this.btnApplyMap.Location = new System.Drawing.Point(84, 382);
+            this.btnApplyMap.Name = "btnApplyMap";
+            this.btnApplyMap.Size = new System.Drawing.Size(103, 48);
+            this.btnApplyMap.TabIndex = 0;
+            this.btnApplyMap.Text = "Copy Map";
+            this.btnApplyMap.UseVisualStyleBackColor = true;
+            this.btnApplyMap.Click += new System.EventHandler(this.btnApplyMap_Click);
+            // 
+            // txtMapName
+            // 
+            this.txtMapName.Location = new System.Drawing.Point(6, 26);
+            this.txtMapName.Name = "txtMapName";
+            this.txtMapName.Size = new System.Drawing.Size(100, 20);
+            this.txtMapName.TabIndex = 2;
+            this.txtMapName.Text = "MapName";
+            this.txtMapName.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = ".udk";
+            // 
+            // chkPackage1
+            // 
+            this.chkPackage1.AutoSize = true;
+            this.chkPackage1.Location = new System.Drawing.Point(182, 85);
+            this.chkPackage1.Name = "chkPackage1";
+            this.chkPackage1.Size = new System.Drawing.Size(15, 14);
+            this.chkPackage1.TabIndex = 3;
+            this.chkPackage1.UseVisualStyleBackColor = true;
+            // 
+            // txtPackage1
+            // 
+            this.txtPackage1.Location = new System.Drawing.Point(7, 82);
+            this.txtPackage1.Name = "txtPackage1";
+            this.txtPackage1.Size = new System.Drawing.Size(152, 20);
+            this.txtPackage1.TabIndex = 4;
+            this.txtPackage1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtPackage2
+            // 
+            this.txtPackage2.Location = new System.Drawing.Point(7, 105);
+            this.txtPackage2.Name = "txtPackage2";
+            this.txtPackage2.Size = new System.Drawing.Size(152, 20);
+            this.txtPackage2.TabIndex = 6;
+            this.txtPackage2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // chkPackage2
+            // 
+            this.chkPackage2.AutoSize = true;
+            this.chkPackage2.Location = new System.Drawing.Point(182, 108);
+            this.chkPackage2.Name = "chkPackage2";
+            this.chkPackage2.Size = new System.Drawing.Size(15, 14);
+            this.chkPackage2.TabIndex = 5;
+            this.chkPackage2.UseVisualStyleBackColor = true;
+            // 
+            // txtPackage3
+            // 
+            this.txtPackage3.Location = new System.Drawing.Point(7, 128);
+            this.txtPackage3.Name = "txtPackage3";
+            this.txtPackage3.Size = new System.Drawing.Size(152, 20);
+            this.txtPackage3.TabIndex = 8;
+            this.txtPackage3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // chkPackage3
+            // 
+            this.chkPackage3.AutoSize = true;
+            this.chkPackage3.Location = new System.Drawing.Point(182, 131);
+            this.chkPackage3.Name = "chkPackage3";
+            this.chkPackage3.Size = new System.Drawing.Size(15, 14);
+            this.chkPackage3.TabIndex = 7;
+            this.chkPackage3.UseVisualStyleBackColor = true;
+            // 
+            // txtPackage4
+            // 
+            this.txtPackage4.Location = new System.Drawing.Point(7, 151);
+            this.txtPackage4.Name = "txtPackage4";
+            this.txtPackage4.Size = new System.Drawing.Size(152, 20);
+            this.txtPackage4.TabIndex = 10;
+            this.txtPackage4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // chkPackage4
+            // 
+            this.chkPackage4.AutoSize = true;
+            this.chkPackage4.Location = new System.Drawing.Point(182, 154);
+            this.chkPackage4.Name = "chkPackage4";
+            this.chkPackage4.Size = new System.Drawing.Size(15, 14);
+            this.chkPackage4.TabIndex = 9;
+            this.chkPackage4.UseVisualStyleBackColor = true;
+            // 
+            // txtPackage5
+            // 
+            this.txtPackage5.Location = new System.Drawing.Point(7, 174);
+            this.txtPackage5.Name = "txtPackage5";
+            this.txtPackage5.Size = new System.Drawing.Size(152, 20);
+            this.txtPackage5.TabIndex = 12;
+            this.txtPackage5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // chkPackage5
+            // 
+            this.chkPackage5.AutoSize = true;
+            this.chkPackage5.Location = new System.Drawing.Point(182, 177);
+            this.chkPackage5.Name = "chkPackage5";
+            this.chkPackage5.Size = new System.Drawing.Size(15, 14);
+            this.chkPackage5.TabIndex = 11;
+            this.chkPackage5.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(164, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Copy?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(5, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Package Names";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 6);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 17);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Map name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(8, 49);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(239, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Packages must be in the same folder as the map!";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(59, 339);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(156, 20);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Map Copying Status:";
+            // 
+            // lblCopyResult
+            // 
+            this.lblCopyResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopyResult.Location = new System.Drawing.Point(63, 359);
+            this.lblCopyResult.Name = "lblCopyResult";
+            this.lblCopyResult.Size = new System.Drawing.Size(152, 20);
+            this.lblCopyResult.TabIndex = 18;
+            this.lblCopyResult.Text = "N/A";
+            this.lblCopyResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblCopyResult.Click += new System.EventHandler(this.lblCopyResult_Click);
+            // 
+            // txtModsDir
+            // 
+            this.txtModsDir.Location = new System.Drawing.Point(6, 23);
+            this.txtModsDir.Name = "txtModsDir";
+            this.txtModsDir.Size = new System.Drawing.Size(241, 20);
+            this.txtModsDir.TabIndex = 19;
+            // 
+            // txtMapDir
+            // 
+            this.txtMapDir.Location = new System.Drawing.Point(6, 67);
+            this.txtMapDir.Name = "txtMapDir";
+            this.txtMapDir.Size = new System.Drawing.Size(241, 20);
+            this.txtMapDir.TabIndex = 20;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(3, 51);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 13);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Map DIR";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(3, 7);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 13);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Mods DIR";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.txtModsDir);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.txtMapDir);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Location = new System.Drawing.Point(6, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(256, 100);
+            this.panel1.TabIndex = 23;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.txtMapName);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.chkPackage1);
+            this.panel2.Controls.Add(this.txtPackage1);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.chkPackage2);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.txtPackage2);
+            this.panel2.Controls.Add(this.txtPackage5);
+            this.panel2.Controls.Add(this.chkPackage3);
+            this.panel2.Controls.Add(this.chkPackage5);
+            this.panel2.Controls.Add(this.txtPackage3);
+            this.panel2.Controls.Add(this.txtPackage4);
+            this.panel2.Controls.Add(this.chkPackage4);
+            this.panel2.Location = new System.Drawing.Point(6, 118);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(256, 218);
+            this.panel2.TabIndex = 24;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 442);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblCopyResult);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnApplyMap);
+            this.Name = "Main";
+            this.Text = "Rocket League Map Copier";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Main_FormClosed);
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnApplyMap;
+        private System.Windows.Forms.TextBox txtMapName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkPackage1;
+        private System.Windows.Forms.TextBox txtPackage1;
+        private System.Windows.Forms.TextBox txtPackage2;
+        private System.Windows.Forms.CheckBox chkPackage2;
+        private System.Windows.Forms.TextBox txtPackage3;
+        private System.Windows.Forms.CheckBox chkPackage3;
+        private System.Windows.Forms.TextBox txtPackage4;
+        private System.Windows.Forms.CheckBox chkPackage4;
+        private System.Windows.Forms.TextBox txtPackage5;
+        private System.Windows.Forms.CheckBox chkPackage5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblCopyResult;
+        private System.Windows.Forms.TextBox txtModsDir;
+        private System.Windows.Forms.TextBox txtMapDir;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ RocketLeagueMapCopier/Main.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RocketLeagueMapCopier
+{
+    public partial class Main : Form
+    {
+
+        public string[] results = new string[] { "Success", "Complete" };
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        // Edit this however you like, just loading default stuff
+        private void Main_Load(object sender, EventArgs e)
+        {
+            txtModsDir.Text = Properties.Settings.Default.modsDir;
+            txtMapDir.Text = Properties.Settings.Default.mapDir;
+            txtMapName.Text = Properties.Settings.Default.mapName;
+            txtPackage1.Text = Properties.Settings.Default.packageName1;
+            txtPackage2.Text = Properties.Settings.Default.packageName2;
+            txtPackage3.Text = Properties.Settings.Default.packageName3;
+            txtPackage4.Text = Properties.Settings.Default.packageName4;
+            txtPackage5.Text = Properties.Settings.Default.packageName5;
+        }
+
+        // Edit this however you like, just storing default stuff
+        private void Main_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Properties.Settings.Default.mapDir = txtMapDir.Text;
+            Properties.Settings.Default.mapName = txtMapName.Text;
+            Properties.Settings.Default.modsDir = txtModsDir.Text;
+            Properties.Settings.Default.packageName1 = txtPackage1.Text;
+            Properties.Settings.Default.packageName2 = txtPackage2.Text;
+            Properties.Settings.Default.packageName3 = txtPackage3.Text;
+            Properties.Settings.Default.packageName4 = txtPackage4.Text;
+            Properties.Settings.Default.packageName5 = txtPackage5.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void btnApplyMap_Click(object sender, EventArgs e)
+        {
+            // Grab the package names (I wish i knew how to make form elements into an array..)
+            List<string> packages = new List<string>();
+            if (chkPackage1.Checked)
+                packages.Add(txtPackage1.Text);
+            if (chkPackage2.Checked)
+                packages.Add(txtPackage2.Text);
+            if (chkPackage3.Checked)
+                packages.Add(txtPackage3.Text);
+            if (chkPackage4.Checked)
+                packages.Add(txtPackage4.Text);
+            if (chkPackage5.Checked)
+                packages.Add(txtPackage5.Text);
+
+            // Copy straight over the existing map
+            string mapOrigFile = Path.Combine(txtMapDir.Text, txtMapName.Text + ".udk");
+            string mapDestFile = Path.Combine(txtModsDir.Text, txtMapName.Text + ".upk");
+            try {
+                File.Copy(mapOrigFile, mapDestFile, true);
+                Console.WriteLine("CopiedMap");
+            } catch (FileNotFoundException) {
+                MessageBox.Show("Map '" + txtMapName.Text + ".udk' Doesn't Exist");
+                lblCopyResult.BackColor = Color.Red;
+                lblCopyResult.Text = "Failed";
+                return;
+            }
+            catch (Exception) {
+                MessageBox.Show("Something's gone wrong. \nCheck your Directories and try again.\n Post on reddit if it persists");
+                lblCopyResult.BackColor = Color.Red;
+                lblCopyResult.Text = "Failed";
+                return;
+            }
+
+            // Copy over the packages
+            foreach(string s in packages)
+            {
+                string packOrigFile = Path.Combine(txtMapDir.Text, s);
+                string packDestFile = Path.Combine(txtModsDir.Text, s);
+
+                try {
+                    File.Copy(packOrigFile, packDestFile, true);
+                } catch (FileNotFoundException) {
+                    lblCopyResult.BackColor = Color.Yellow;
+                    lblCopyResult.Text = "Partial Success";
+                    MessageBox.Show("'" + s + "' Doesn't exist.\n" + 
+                                    "Make sure you included '.upk' in your file name.\n" + 
+                                    "Package not copied.");
+                }
+                catch (ArgumentException) {
+                    lblCopyResult.BackColor = Color.Yellow;
+                    lblCopyResult.Text = "Partial Success";
+                    MessageBox.Show("Copy requested on package without a name given.\nPackage not copied.");
+                }
+                catch (IOException) {
+                    lblCopyResult.BackColor = Color.Yellow;
+                    lblCopyResult.Text = "Partial Success";
+                    MessageBox.Show("Copy requested on package without a name given.\nPackage not copied.");
+                }
+            }
+
+            // Make sure there weren't complications
+            if (lblCopyResult.BackColor != Color.Yellow && lblCopyResult.BackColor != Color.Red)
+            {
+                lblCopyResult.BackColor = Color.LightGreen;
+
+                // Notify of completion (multiple values so they know it's worked when they re-copy it again.)
+                if (lblCopyResult.Text == results[0])
+                    lblCopyResult.Text = results[1];
+                else
+                    lblCopyResult.Text = results[0];
+            }
+        }
+
+        private void lblCopyResult_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
